b38b2fca7a6c5fff172f274888005fd456e949bf
Iamankitsarkar <ankitneil@yahoo.com>
2016-04-21 12:16:26 -0400

    testing

--- Boot.js
+++ Boot.js
@@ -8,20 +8,5 @@ MonkeyDefense.Boot.prototype = {
     },
 
 
-create: function () {
-		this.input.maxPointers = 10;
-		this.stage.disableVisibilityChange = false;
-		this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-		this.scale.minWidth = 270;
-		this.scale.minHeight = 480;
-		this.scale.pageAlignHorizontally = true;
-		this.scale.pageAlignVertically = true;
-		this.stage.forcePortrait = true;
-		this.scale.setScreenSize(true);
 
-		this.input.addPointer();
-		this.stage.backgroundColor = '#171642';
-        
-        this.state.start('Preloader');
-	}
 };
