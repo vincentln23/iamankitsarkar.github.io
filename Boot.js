cc6a8d43456284ca0d6e53cd066c43a5d58a62cb
vincentln23 <vincentln23@gmail.com>
2016-04-21 12:19:55 -0400

    Update Boot.js

--- Boot.js
+++ Boot.js
@@ -10,3 +10,20 @@ MonkeyDefense.Boot.prototype = {
 
 
 };
+
+	create: function() {
+		this.input.maxPointers = 1;
+		this.stage.disableVisibilityChange = false; // pause game on tab change
+		this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+		this.scale.minWidth = 270;
+		this.scale.minHeight = 480;
+		this.scale.pageAlignHorizontally = true;
+		this.scale.pageAlignVertically = true;
+		this.stage.forcePortrait = true;  // force portrait mode
+		this.scale.setScreenSize(true);  // true will force screen resize no matter what
+
+		this.input.addPointer();
+		this.stage.backgroundColor = '#171642';
+	}
+	
+};
