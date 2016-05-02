0e323e2df9c078a66495eea99d0c904b859bce50
Iamankitsarkar <ankitneil@yahoo.com>
2016-05-02 13:44:38 -0400

    worked on code

--- Boot.js
+++ Boot.js
@@ -7,10 +7,25 @@ MonkeyDefense.Boot.prototype = {
         this.load.image('Monkey Defense Title', 'C:\\Users\\1381362\\Google Drive\\AdvProgramming Project_\\Master\\Image assets\\monkeydefensetitle.png')
     },
 
+    create: function () {
+		this.input.maxPointers = 10;
+		this.stage.disableVisibilityChange = false;
+		this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+		this.scale.minWidth = 270;
+		this.scale.minHeight = 480;
+		this.scale.pageAlignHorizontally = true;
+		this.scale.pageAlignVertically = true;
+		this.stage.forcePortrait = true;
+		this.scale.setScreenSize(true);
+
+		this.input.addPointer();
+		this.stage.backgroundColor = '#33FF00';
+        
+        this.state.start('Preloader');
+	}
 
 
 };
-
 	create: function() {
 		this.input.maxPointers = 1;
 		this.stage.disableVisibilityChange = false; // pause game on tab change
---
+++ Preloader.js
@@ -0,0 +1,11 @@
+BunnyDefender.Preloader = function (game) {
+    this.preloadBar = null;
+    this.titleText = null;
+    this.ready = false;
+};
+
+BunnyDefender.Preloader.prototype = {
+    preload: function (){
+       
+  
+}
